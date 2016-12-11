36a8012885e0dd0f8f73733a31c67e9479a33e59
Pablo Lago <plago-bergonpazos@danielcastelao.org>
2016-12-11 20:12:37 +0000

    Tercer commit: Error 404 hecho

---
+++ 404.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri ();?>"/>  
+    </head>
+    
+    <body>
+        
+    <div id="error404">
+        <h1>ERROR 404: Page not found.</h1>
+        <img src="<?php echo get_template_directory_uri() ?>/imagenes/travolta.gif" ALT="travolta">
+        <h2>Cuando haces el examen de SxE y suspendes...</h2>
+    </div>
+
+   </body>
+</html>
