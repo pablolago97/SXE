ce57dc645726005c152e05e8eb593bc542d1b85d
Pablo Lago <plago-bergonpazos@danielcastelao.org>
2016-12-11 20:05:10 +0000

    Primer commit: header y footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+       <?php wp_nav_menu(
+           array(
+            'menu' => 'menu4'
+        ) ); ?>
+        
+        <?php wp_nav_menu(
+           array(
+            'menu' => 'menu5'
+        ) ); ?>
+        
+        <?php wp_nav_menu(
+           array(
+            'menu' => 'menu6'
+        ) ); ?>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+       <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/style.css';?>"/>  
+       <link rel="icon" type="image/jpg" href="<?php echo get_template_directory_uri() ?>/imagenes/photo.jpg" ALT="photo" />
+    </head>
+  
+<body>
+    
+    <div id="cabecera">
+           <img src="<?php echo get_template_directory_uri() ?>/imagenes/cabecera.png" ALT="cabecera">
+    </div>
+    
+    <h2 id=entrada>•Entradas•</h2>
+    
+    
+
+    
+
+
+
